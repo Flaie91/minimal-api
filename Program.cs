d7f3b4792cfae8c04193e4db0a3210881dd7f30c
Flaie <flaie9@gmail.com>
2024-10-30 17:01:57 -0300

    Finalizado CRUD

--- Program.cs
+++ Program.cs
@@ -1,16 +1,184 @@
 using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Enuns;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.ModelViews;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DBcontexto>(options =>{
+    options.UseMySql(builder.Configuration.GetConnectionString("mysql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
+});
+
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => "Hello World!");
+#region Home
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+#endregion
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => 
+#region Administradores
+app.MapPost("/Administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => 
 {
-  if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+  if(administradorServico.Login(loginDTO) != null)
       return Results.Ok("Login com sucesso");
   else
       return Results.Unauthorized();
-});
+}).WithTags("Administradores");
 
+app.MapPost("/Administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) => 
+{
+  var validacao = new ErrosDeValidacao{
+    Mensagens = new List<string>()};
+  
+  if(string.IsNullOrEmpty(administradorDTO.Email)) validacao.Mensagens.Add("Não é possível incluir email vazio!");
+  if(string.IsNullOrEmpty(administradorDTO.Senha)) validacao.Mensagens.Add("Não é possível incluir Senha vazia!");
+  if(administradorDTO.Perfil == null) validacao.Mensagens.Add("Não é possível incluir Perfil vazia!");
+  
+  if(validacao.Mensagens.Count > 0) return Results.BadRequest(validacao);
+
+  var administrador = (new Administrador{
+    Email = administradorDTO.Email,
+    Senha = administradorDTO.Senha,
+    Perfil = administradorDTO.Perfil.ToString() ?? Perfil.editor.ToString()
+
+});
+  administradorServico.Incluir(administrador);
+
+  return Results.Created($"/administrador/{administrador.Id}", new AdministradorModelView{
+      Id = administrador.Id,
+      Email = administrador.Email,
+      Perfil = administrador.Perfil
+  });
+
+}).WithTags("Administradores");
+
+app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico administradorServico) => 
+{
+  var adms = new List<AdministradorModelView>();
+  var administradores = administradorServico.Todos(pagina);
+  foreach (var adm in administradores)
+  {
+    adms.Add(new AdministradorModelView{
+      Id = adm.Id,
+      Email = adm.Email,
+      Perfil = adm.Perfil
+    });
+  }
+
+  return Results.Ok(adms);
+}).WithTags("Administradores");
+
+app.MapGet("/administradores/{id}", ([FromRoute] int id, IAdministradorServico administradorServico) => 
+{
+  var administrador = administradorServico.BuscaPorId(id);
+  if(administrador == null) return Results.NotFound();
+  return Results.Ok(new AdministradorModelView{
+      Id = administrador.Id,
+      Email = administrador.Email,
+      Perfil = administrador.Perfil
+  });
+}).WithTags("Administradores");
+
+#endregion
+
+#region Veiculos
+ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
+{
+    var validacao = new ErrosDeValidacao{
+      Mensagens = new List<string>()
+    };
+
+  if(string.IsNullOrEmpty(veiculoDTO.Nome)) 
+    validacao.Mensagens.Add("O nome não pode ser vazio!");
+
+  if(string.IsNullOrEmpty(veiculoDTO.Marca)) 
+    validacao.Mensagens.Add("A Marca não pode estar em branco!");
+
+  if(veiculoDTO.Ano < 1950 || veiculoDTO.Ano > (DateTime.Now.Year + 1))   
+    validacao.Mensagens.Add("Não é possivel incluir data inferior a 1950 ou superior ao próximo ano!");
+
+    return validacao;
+}
+
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => 
+{    
+  var validacao = validaDTO(veiculoDTO);
+  if(validacao.Mensagens.Count > 0) return Results.BadRequest(validacao);
+
+  var veiculo = (new Veiculo{
+    Nome = veiculoDTO.Nome,
+    Marca = veiculoDTO.Marca,
+    Ano = veiculoDTO.Ano
+  });
+  veiculoServico.Incluir(veiculo);
+
+  return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+
+}).WithTags("Veiculos");
+
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) => 
+{
+  var veiculos = veiculoServico.Todos(pagina);
+   
+  return Results.Ok(veiculos);
+
+}).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => 
+{
+  var veiculo = veiculoServico.BuscaPorId(id);
+  if(veiculo == null) return Results.NotFound();
+  return Results.Ok(veiculo);
+
+}).WithTags("Veiculos");
+
+app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => 
+{
+  var veiculo = veiculoServico.BuscaPorId(id);
+  if(veiculo == null) return Results.NotFound();
+
+  var validacao = validaDTO(veiculoDTO);
+  if(validacao.Mensagens.Count > 0) return Results.BadRequest(validacao);
+  
+  veiculo.Nome = veiculoDTO.Nome;
+  veiculo.Marca = veiculoDTO.Marca;
+  veiculo.Ano = veiculoDTO.Ano;
+
+  veiculoServico.Atualizar(veiculo);
+
+  return Results.Ok(veiculo);
+
+}).WithTags("Veiculos");
+
+app.MapDelete("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => 
+{
+  var veiculo = veiculoServico.BuscaPorId(id);
+  if(veiculo == null) return Results.NotFound();  
+
+  veiculoServico.Apagar(veiculo);
+
+  return Results.NoContent();
+
+}).WithTags("Veiculos");
+
+#endregion
+
+#region App
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
+#endregion
